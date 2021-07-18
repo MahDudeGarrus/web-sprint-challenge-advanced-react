4944efb5008fc48694cca54be9c46142c5e18007
Stephanie Ann Solis <stephaniesolis@lambdastudents.com>
2021-07-18 17:14:27 -0600

    created useForm hook, imported to CheckOutForm, and moved states and handlers to useForm

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,23 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+const useForm = (initialValues) => {
+    const [values, setValues] = useState(initialValues);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+    const handleChanges = (event) => {
+        setValues({
+            ...values,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+      };
+
+    return [values, handleChanges, showSuccessMessage, handleSubmit];
+}
+
+export default useForm;
