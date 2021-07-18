000a1ee368bb3bb30a8a8337ca3c1371c6f7a102
Stephanie Ann Solis <stephaniesolis@lambdastudents.com>
2021-07-18 16:04:25 -0600

    initial commit, testing connection for codegrade

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import CheckoutForm from "./components/CheckoutForm";
 
 import "./App.css";
 
+//checking codegrade connection with this comment.
+
 function App() {
   // array of plants that have been added to the cart
   const [cart, setCart] = useState([]);
